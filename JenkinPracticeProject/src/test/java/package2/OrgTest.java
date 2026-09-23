e29f7c24a7a42cbd5aa2c54222a63679b68e4225
DeepaMushtoor <deepamushtoor111@gmail.com>
2026-09-23 15:05:47 +0530

    FIRSTJenkinPush

---
+++ JenkinPracticeProject/src/test/java/package2/OrgTest.java
@@ -0,0 +1,24 @@
+package package2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+
+	
+	@Test
+	public void createOrgTest()
+	{
+		
+		System.out.println("Executed createOrgTest");
+		
+		
+	}
+	@Test
+	public void modifyOrgTest()
+	{
+		
+		System.out.println("Executed modifyOrgTest");
+		
+		
+	}
+}
